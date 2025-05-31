b1c65622a7a3efabbc54b1c3ae832a5b9d60ee2d
Saara Khan <itsaarakhan@gmail.com>
2025-05-31 21:39:03 +0530

    Task-1 Completed

---
+++ Task-1 [Library Management System]/02_queries_information_retrieval.sql
@@ -0,0 +1,9 @@
+-- a) Books borrowed by a specific member
+
+-- b) Members with overdue books (>30 days not returned)
+
+-- c) Books by genre with count of available copies
+
+-- d) Most borrowed book(s)
+
+-- e) Members who borrowed books from at least 3 genres
